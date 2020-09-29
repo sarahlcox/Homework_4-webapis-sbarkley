342fa1b56831a5d607b39b352be512564adc020b
sarahlcox <sarahl.cox.22@gmail.com>
2020-09-29 15:35:58 -0500

    added basic layout in index and started pseudocoding in script

---
+++ script.js
@@ -0,0 +1,18 @@
+// First create prompt to start quiz
+// First create array of questions
+var questions = [
+    {
+    prompt: "What color is the sky?\n(a)purple \n(b)blue \n(c)red",
+    answer: "b"
+    },
+    {
+        prompt: "What color is the grass?\n(a)green \n(b)blue \n(c)red",
+        answer: "a"
+        },
+]
+var score = 0;
+
+// Create for loop to ask questions to player
+for (var i=o; i <questions.length; i++){
+
+}
