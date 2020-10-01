e47deceadd50b78466d6a3fe1392803f3e7e5816
sarahlcox <sarahl.cox.22@gmail.com>
2020-09-30 19:17:44 -0500

    added a new backup of old codes that weren't working, and started new code, added more pseudocode to script.js, updated index.html

--- script.js
+++ script.js
@@ -1,18 +1,85 @@
-// First create prompt to start quiz
-// First create array of questions
-var questions = [
-    {
-    prompt: "What color is the sky?\n(a)purple \n(b)blue \n(c)red",
-    answer: "b"
-    },
-    {
-        prompt: "What color is the grass?\n(a)green \n(b)blue \n(c)red",
-        answer: "a"
-        },
-]
+//working in a study group with Matt Hiatt, Erik Tomlinson, and Jake Wagner
+// declare function .timeEl - call jquery 
+// declare global variables - user score, quiz container, high score
+// First function to to start quiz and timer 
+// Timer starts when quiz starts (set interval)  setInterval(someFunction, seconds*1000) 
+var test = document.getElementById('test');
+var timer = document.getElementById('timer');
+var buttonContainer = document.getElementById('buttonContainer');
+var questionContainer = document.getElementById('questionContainer');
+
+// declare someFunction[Create array of questions as objects - questions, options, and/or [booleans]
+// array starts for loop identigy the someFunction, i<x, i++ (doesnt have to be "<" symbol Use === to have a concrete true answer)
+    // Loop through array --> for (var i=0; i<array.length; i++) {
+        // One Question
+        // Use If/else
+        // userScore ++/--
 var score = 0;
-
-// Create for loop to ask questions to player
-for (var i=o; i <questions.length; i++){
-
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('submit');
+var questions = [
+        {
+            question: "Question 1?",
+            choices: ["1", "2", "3", "4"],
+            answer: 0
+          },
+          {
+            question: "Question 2?",
+            choices: ["1", "2", "3", "4"],
+            answer: 1
+          },
+];
+// this starts the timer
+function begin(){
+  // console.log("it works") 
+  buttonContainer.setAttribute("class", "hide")
+  var timerLogic = 60;
+  var timerInterval = setInterval(function(){
+      timerLogic--
+      timer.textContent = timerLogic
+      //if the timer reaches 0 alert pops up that time is up
+      if(timerLogic <=0){
+          clearInterval(timerInterval)
+          alert("Time's Up!")
+      }
+  },1000)
 }
+//this is the behavior behind the begin button
+test.addEventListener('click', begin)
+
+// Rotation of questions for the quiz, within our question array, we need to pull the objects  from the array
+
+// adds points to score if correct
+// adds points to score if incorrect
+*********
+function nextQuestion (){
+  //first question presented, if you get the question right, you get a point, if it matches 1 of the 3 wrong answers, move on with no point added, deduct time from clock
+ 
+
+
+//create function for all right answers, and come up with code to give user 1 point, and have the question go on to next question
+
+
+// function quiz {
+//   var question1= questions.length
+//   if (condition) {
+    
+//   } else {
+    
+//   }
+
+// }
+
+
+// Create for loop to ask questions to player .length use === to have a concrete true answer
+//reference: https://stackoverflow.com/questions/37252041/storing-quiz-questions-in-array-of-objects
+// for (let index = 0; index < questions.length; index++) {
+//     var question = questions[index].question;
+//     document.write ( question );
+//     console.log(question);
+    
+// }
+
+
+// alert("you got " + score + "/ + questions.length");
+
